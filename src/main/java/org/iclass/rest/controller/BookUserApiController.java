7068347ec213538bc0580e790f5dd904c67a033f
limJaemin99 <limjaemin99@gmail.com>
2023-10-12 08:54:20 +0900

    Spring-boot boot6rest Day02
    ● Spring-boot 동기/비동기 연습 및 JSON 연습
    - @PatchMapping 연습
    - @DeleteMapping 연습

--- src/main/java/org/iclass/rest/controller/BookUserApiController.java
+++ src/main/java/org/iclass/rest/controller/BookUserApiController.java
@@ -35,6 +35,7 @@ public class BookUserApiController {
 	@PostMapping("/bookuser")	//요청에는 헤더와 바디가 있다. @RequestBody 는 bookUser 가 요청의 body 라고 알려준다.
 	public Map<String,Integer> save(@RequestBody @Valid BookUser bookUser){	//▲ 클라이언트가 보낸 json 문자열을 자바 객체로 자동 변환 (★역직렬화)
 		log.info("━━━━━━━━━━ request body : {}",bookUser);
+
 		int count = bookUserMapper.insert(bookUser);
 		Map<String,Integer> resultMap = new HashMap<>();	//처리 결과를 응답하기 위한 Map
 		resultMap.put("count",count);
@@ -44,18 +45,61 @@ public class BookUserApiController {
 
 	@GetMapping("/bookuser/{id}")
 	public BookUser selectOne(@PathVariable String id){
-		BookUser bookUser = bookUserMapper.selectOne(id);
 		log.info("━━━━━━━━━━ path variable id : {}",id);
 
+		BookUser bookUser = bookUserMapper.selectOne(id);
+
 		return bookUser;	//bookUser DTO 를 json 문자열로 변환시켜 전달한다. (★직렬화)
+	}
+	@GetMapping("/bookuser/check/{id}")
+	public Map<String,Boolean> check(@PathVariable String id){
+		log.info("━━━━━━━━━━ path variable id : {}",id);
+
+		int count = bookUserMapper.isExist(id);
+		Map<String,Boolean> resultMap = new HashMap<>();
+		resultMap.put("exist",count == 1);
+
+		return resultMap;	//bookUser DTO 를 json 문자열로 변환시켜 전달한다. (★직렬화)
 	}
 
 	@DeleteMapping("/bookuser/{id}")
 	public Map<String, Integer> delete(@PathVariable String id){
 		log.info("━━━━━━━━━━ path variable id : {}",id);
+
 		int result = bookUserMapper.delete(id);
 		Map<String,Integer> resultMap = new HashMap<>();
 		resultMap.put("delete result",result);
+
+		return resultMap;
+	}
+
+	@PatchMapping("/bookuser/{field}/{id}")
+	public Map<String,Object> changeOneField(@PathVariable String field,
+											 @PathVariable String id,
+											 // ▼ DTO에 담는 이유 : 데이터는 1개이지만 유효성 검사를 위해
+											 @RequestBody @Valid BookUser bookUser){
+		Map<String,Object> map = new HashMap<>();
+		map.put("field",field);
+		map.put("id",id);
+		switch (field){
+			case "email" -> map.put("value",bookUser.getEmail());
+			case "birth" -> map.put("value",bookUser.getBirth());
+			case "password" -> map.put("value",bookUser.getPassword());
+			case "subjects" -> map.put("value",bookUser.getSubjects());
+		}
+
+		int count = bookUserMapper.changeOneField(map);
+		map.put("count",count);
+
+		return map;
+	}
+
+	@PatchMapping("/bookuser/{id}")
+	public Map<String,Integer> update(@RequestBody @Valid BookUser bookUser){
+		int count = bookUserMapper.changeMany(bookUser);
+
+		Map<String,Integer> resultMap = new HashMap<>();
+		resultMap.put("count",count);
 
 		return resultMap;
 	}
--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -7,10 +7,17 @@ const clear = function(){
     document.querySelector('#birth').value='1999-01-01'
     document.querySelector('#regdate').value=''
     document.querySelectorAll('.subjects').forEach(item => item.checked =false)
-    document.querySelector('#default').innerHTML =`<div class="card-header" id="chatBot">나는 챗봇입니다.</div>`
+    document.querySelector('#default').innerHTML =`<div class="card-header" id="chatBot">여기에 메시지가 나타납니다.</div>`
+    document.querySelector('#idMessage').innerHTML = `<span class="text-sm"></span>`
 }
 
 document.querySelector('#clear').addEventListener('click',clear)
+//콜백함수는 메소드 또는 함수안에서 인자로 사용되는 함수                            ㄴ 이게 콜백 함수
+//addEventListener 는 클릭이 발생하면 실행시킬 clear 함수를 등록합니다.(브라우저에게 알려줌)
+//브라우저는 #clear 의 클릭이 발생하는지 listener(감시)하고 있다가 사용자가 클릭하면
+// clear 함수를 큐(자료구조)에 저장 합니다. (FIFO - 선입선출)
+//브라우저 스케줄링을 통해서 큐에 저장된 함수들을 순서대로 실행시켜 줍니다.
+//큐에 저장하는 순서와 실행 순서는 개발자가 예측한 순서와 다를 수 있습니다.
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
 
@@ -67,6 +74,7 @@ const selectOne = function() {
         if(xhr.status === 200 || xhr.status ===201){
             const result = JSON.parse(xhr.response)
             resultOne(result)
+            document.querySelector('#idMessage').innerHTML = `<span class="text-sm"></span>`
         } else {
             console.error('오류1',xhr.status)
             console.error('오류2',xhr.response)
@@ -81,13 +89,17 @@ const resultOne = function(result){
     document.querySelector('#email').value=result.email
     document.querySelector('#birth').value=result.birth
     document.querySelector('#regdate').value=result.reg_date
+    arrSubject.splice(0,arrSubject.length)
     document.querySelectorAll('.subjects').forEach(item => {
         // if(result.subjects.indexOf(item.value) >= 0) //indexOf ▶ return int
         //     item.checked = true
-        if(result.subjects.includes(item.value))    //includes ▶ return boolean
+        if(result.subjects.includes(item.value)) {    //includes ▶ return boolean
             item.checked = true                     //contains ▶ js에서 사용 불가 X
+            arrSubject.push(item.value) //조회한 관심 분야로 배열 초기화
+        } else
+            item.checked = false
     })
-    document.querySelector('#default').innerHTML =`<div class="card-header" id="chatBot">나는 챗봇입니다.</div>`
+    document.querySelector('#default').innerHTML =`<div class="card-header" id="chatBot">여기에 메시지가 나타납니다.</div>`
 }
 
 document.querySelector('#selectOne').addEventListener('click',selectOne)
@@ -96,7 +108,7 @@ document.querySelector('#selectOne').addEventListener('click',selectOne)
 
 //관심분야 선택 check 로 문자열 만들기
 const arrSubject = []   //arrSubject.toString() 은 배열 요소로 문자열을 만들어준다. (★ POST 에서 사용하기)
-const checkSubject = e => {
+const checkSubject = e => { // == const checkSubject = function(e) {
     e.stopPropagation()
 
     const target = e.target
@@ -107,7 +119,7 @@ const checkSubject = e => {
         const index = arrSubject.indexOf(target.value);   //해당 값의 배열 위치를 알아내기
         if(index !== -1) arrSubject.splice(index,1)     //해당 위치에서 삭제하기
     }
-    console.log(arrSubject) //클릭하면서 결과를 콘솔에서 확인하세요
+    // console.log(arrSubject) //클릭하면서 결과를 콘솔에서 확인하세요
 }
 
 //id 'checkSubjects'는 checkbox input 모두를 포함하고 있는 div 태그이다.
@@ -116,39 +128,252 @@ document.querySelector('#checkSubjects').addEventListener('click',checkSubject)
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
 
-//회원 등록 : POST
-const save = function(){
+//ID 중복확인 함수 : idCheck()
+let isValid = false   //리턴 변수
+function idCheck() {    //함수 선언을 이렇게 하면 끌어올리기 가능 O
+    const id = document.querySelector('#id').value
+
+    if(!id) {
+        alert('아이디를 입력하세요')
+        document.querySelector('#idMessage').innerHTML = `<span class="text-sm"></span>`
+        return
+    }
+
     const xhr = new XMLHttpRequest()    //비동기 통신 객체 생성
+    xhr.open('GET','/api/bookuser/check/'+id)   //전송 보낼 준비 (URL 과 method)
+    xhr.send()
+    xhr.onload = function (){
+        if(xhr.status === 200 || xhr.status === 201){
+            //json 문자열 ▶ 자바스크립트 객체
+            const result = JSON.parse(xhr.response) //map 객체를 parse(★역직렬화)
+            console.log('응답 : ',result.exist)
+            //서버 응답 exist 값으로 isValid 저장. 존재하면 새로운 회원은 사용할 수 없는 아이디
+            isValid = !result.exist   //result.exist 리턴타입 : boolean
+            let resultMsg
+            let resultColor
+            if (isValid){
+                resultMsg = '없는 ID 입니다. 회원가입이 가능합니다.'
+                resultColor = 'blue'
+            } else {
+                resultMsg = '중복된 ID 입니다. 회원가입이 불가능합니다.'
+                resultColor = 'red'
+            }
+            document.querySelector('#idMessage > span').innerHTML = resultMsg
+            document.querySelector('#idMessage > span').style.color = resultColor
+        } else {
+            console.error('오류',xhr.status,xhr.response)
+        }
+    }
+    return isValid    //사용할 수 있으면 true
+}
+
+document.querySelector('#id').addEventListener('keyup',idCheck)
+
+//회원 등록 : POST
+const save = function(){    //리터럴 형식의 함수 선언 (끌어올리기 불가능 X , 변수처럼 사용하기 전에 선언)
+    const xhr = new XMLHttpRequest()    //비동기 통신 객체 생성
+
     const id = document.getElementById('id').value
+    //ID 중복확인
+
+    let yn = false
+    if(id.length == 0){
+        alert('ID 는 공백이 될 수 없습니다.')
+        return
+    } else if(!isValid){
+        alert('이미 사용중인 ID 입니다.')
+        return
+    } else {
+        yn = confirm(`ID '${id}' 로 회원가입 하시겠습니까?`)
+        if(!yn) return  //confirm 에서 취소 누르면 false
+    }
+
+    //1. 입력값 가져오기
     const name = document.getElementById('name').value
     const email = document.getElementById('email').value
     const birth = document.getElementById('birth').value
     const password = document.getElementById('password').value
 
+    //2. 입력값으로 자바스크립트 객체 생성 (자바스크립트 객체는 미리 타입을 정의하지 않고 사용할 수 있다.)
     let json = {
-        birth:birth,
-        email:email,
         id:id,
         name:name,
         password:password,
+        birth:birth,
+        email:email,
         subjects:arrSubject.toString()
     }
 
+    //3. 자바스크립트 객체를 json 전송을 위해 ★직렬화 (문자열로 변환)
+    const jsStr = JSON.stringify(json)
+
+    //4. 전송 보낼 준비 (URL 과 method)
     xhr.open('POST','/api/bookuser')
     xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8");
-    xhr.send(JSON.stringify(json))
+    xhr.send(jsStr) //5. 요청 전송
     xhr.onload = function() {
+        const resultObj = JSON.parse(xhr.response)
         if(xhr.status === 200 || xhr.status === 201){
-            const result = JSON.parse(xhr.response)
-            if(result.count == 1)
-                document.querySelector('#default').innerHTML =`<div class="card-header" id="chatBot">${id}님이 등록되었습니다.</div>`
+            if(resultObj.count == 1){
+                clear() //정상 등록 후 입력 요소 초기화
+                document.querySelector('.card-header').innerHTML = '새로운 회원 ['+id+'] 가입되었습니다.'
+                document.querySelector('.card-header').style.color = 'blue'
+            }
         } else {
-            console.error('오류2',xhr.response.indexOf('name'))
-            let str;
-            // if(xhr.response.indexOf('name'))
+            console.error('오류1',xhr.status)
+            console.error('오류2',xhr.response)
 
+            const values = Object.values(resultObj) //자바스크립트 객체는 key , value 구성. 그 중에 value만 가져와서 배열로 만든다.
+            let resultMsg = ''
+            values.forEach(msg => resultMsg += msg + "<br>")
+
+            document.querySelector('.card-header').innerHTML = resultMsg
+            document.querySelector('.card-header').style.color = 'red'
+        }
+    }
+    setTimeout(clear,7000)  //1000 = 1초
+}
+
+document.querySelector('#save').addEventListener('click',save)
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
+
+//회원 삭제 : DELETE
+const deleteUser = function () {
+    const xhr = new XMLHttpRequest()
+
+    const id = document.getElementById('id').value
+
+    xhr.open('DELETE','/api/bookuser/'+id)
+    xhr.send()
+    xhr.onload=function(){
+        const result = JSON.parse(xhr.response)
+        let rstCount = Object.values(result)
+        if((xhr.status === 200 || xhr.status ===201) && rstCount == 1){
+            document.querySelector('.card-header').innerHTML = '['+id+'] 회원이 삭제되었습니다.'
+            document.querySelector('.card-header').style.color = 'blue'
+        } else {
+            console.error('오류1',xhr.status)
+            console.error('오류2',xhr.response)
+
+            let resultMsg = '삭제 실패 : '
+
+            if(id.length == 0)
+                resultMsg += 'ID 를 입력하세요'
+            else
+                resultMsg += '존재하지 않는 ID'
+
+            document.querySelector('.card-header').innerHTML = resultMsg
+            document.querySelector('.card-header').style.color = 'red'
+        }
+    }
+    setTimeout(clear,7000)  //1000 = 1초
+}
+
+document.querySelector('#delete').addEventListener('click',deleteUser)
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
+
+//실제 프로젝트 할 때는 패스워드 , 이메일 변경 1개 필드를 변경할 때 [모달]을 사용하여 입력값 받기
+//1개씩 변경하기 버튼
+const changeOneField = function (e) {
+    e.stopPropagation()
+    const target = e.target
+
+    if (target.tagName != 'BUTTON') return
+
+    const field = target.getAttribute("data-num")
+    const id = document.querySelector('#id').value  //where 에 필요한 id
+    let value = ''
+
+    if(field == 'subjects') //변경하려는 필드가 '관심분야' 일 경우
+        value = arrSubject.toString()
+    else
+        value = document.getElementById(field).value    //field = 변수명
+
+    console.log("field : ",field)
+    console.log("value : ",value)
+
+    const jsObj = {
+        id:id   //① id : 프로퍼티 이름 ② id : 변수명
+    }
+    jsObj[field] = value    //jsObj 객체에 새로운 프로퍼티 field와 그 값 추가
+    console.log("jsObj 중간 확인 : ",jsObj)
+
+    const jsStr = JSON.stringify(jsObj)
+    console.log(jsStr)
+
+    const xhr = new XMLHttpRequest()
+    xhr.open('PATCH','/api/bookuser/'+field+'/'+id)
+    xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8")
+    xhr.send(jsStr)
+    xhr.onload = function () {
+        const result = JSON.parse(xhr.response)
+        if (xhr.status === 200 || xhr.status === 201){
+            if(result.count == 1){
+                document.querySelector('.card-header').innerHTML = '['+id+'] 님의 ['+ field +'] 정보 수정이 완료되었습니다.'
+                document.querySelector('.card-header').style.color = 'blue'
+            } else {
+                console.log('오류1-',xhr.response)
+                console.log('오류2-',xhr.status)
+
+                const values = Object.values(result);
+                console.log(values)
+
+                let resultMsg =''
+                values.forEach(msg => resultMsg += msg +"<br>")
+
+                document.querySelector('.card-header').innerHTML = resultMsg
+            }
         }
     }
 }
 
-document.querySelector('#save').addEventListener('click',save)
+document.querySelector('.card-body').addEventListener('click',changeOneField)
+
+//━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
+
+//여러개 변경 : PATCH
+const update = function (){
+    const xhr = new XMLHttpRequest()
+    const id = document.getElementById('id').value
+    const password = document.getElementById('password').value
+    const email = document.getElementById('email').value
+    const birth = document.getElementById('birth').value
+
+    let json = {
+        id:id,
+        password:password,
+        birth:birth,
+        email:email,
+        subjects:arrSubject.toString()
+    }
+
+    const jsStr = JSON.stringify(json)
+
+    xhr.open('PATCH','/api/bookuser/'+id)
+    xhr.setRequestHeader("Content-Type","application/json; charset=UTF-8");
+    xhr.send(jsStr)
+    xhr.onload = function (){
+        const result = JSON.parse(xhr.response)
+        if(xhr.status === 200 || xhr.status === 201){
+            if(result.count == 1){
+                clear()
+                document.querySelector('.card-header').innerHTML = '['+id+'] 님의 정보가 수정되었습니다.'
+                document.querySelector('.card-header').style.color = 'blue'
+            }
+        } else {
+            console.error('오류1',xhr.status)
+            console.error('오류2',xhr.response)
+
+            const values = Object.values(result)
+            let resultMsg = ''
+            values.forEach(msg => resultMsg += msg + "<br>")
+
+            document.querySelector('.card-header').innerHTML = resultMsg
+            document.querySelector('.card-header').style.color = 'red'
+        }
+    }
+}
+
+document.querySelector('#update').addEventListener('click',update)
